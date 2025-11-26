74c0361d250fcbb5dfade029b882b500d8954813
MoonGlitters <allen_nivek@hotmail.com>
2025-11-26 16:12:35 -0300

    Hot Fix

--- servidor/servicios/auth.servicio.js
+++ servidor/servicios/auth.servicio.js
@@ -29,7 +29,7 @@ export const loginUsuario = async (email, password) => {
 
     const now = new Date();
     if (usuario.lock_until && now < usuario.lock_until) {
-        const remaining = Math.ceil((user.lock_until - now) / 60000);
+        const remaining = Math.ceil((usuario.lock_until - now) / 60000);
         throw new Error(`Cuenta bloqueada. Intenta en ${remaining} minutos.`)
     }
 
@@ -40,7 +40,7 @@ export const loginUsuario = async (email, password) => {
             usuario.lock_until = new Date(Date.now() + 1 * 60 * 1000);
         }
         await usuario.save();
-        throw new Error(`Credenciales inválidas ${Math.max(0, 3 - usuario.failed_attempts)}`);}
+        throw new Error(`Credenciales inválidas. Intentos Restantes: ${Math.max(0, 3 - usuario.failed_attempts)}`);}
 
     //  Bloquear si esta inactivo
     if (usuario.estado === "inactiva") {
